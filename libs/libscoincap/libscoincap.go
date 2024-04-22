3b9498ee8e02f166904f04d157519edd7fc92beb
adhiyatmarama <adhiyatma.pramayoga@gmail.com>
2024-04-22 15:17:40 +0700

    chore: refactor type coin and some function

--- libs/libscoincap/libscoincap.go
+++ libs/libscoincap/libscoincap.go
@@ -2,30 +2,29 @@ package libscoincap
 
 import (
 	"encoding/json"
+	"errors"
 	"log"
 	"net/http"
+	"net/url"
 )
 
 var BASE_URL = "https://api.coincap.io/v2"
 
 type Coin struct {
-	Id                string `json:"id"`
-	Rank              string `json:"rank"`
-	Symbol            string `json:"symbol"`
-	Name              string `json:"name"`
-	Supply            string `json:"supply"`
-	MaxSupply         string `json:"maxSupply"`
-	MarketCapUsd      string `json:"marketCapUsd"`
-	VolumeUsd24Hr     string `json:"volumeUsd24Hr"`
-	PriceUsd          string `json:"priceUsd"`
-	ChangePercent24Hr string `json:"changePercent24Hr"`
-	Vwap24Hr          string `json:"vwap24Hr"`
-	Explorer          string `json:"explorer"`
+	Id       string `json:"id"`
+	Name     string `json:"name"`
+	PriceUsd string `json:"priceUsd"`
+	PriceIdr string `json:"priceIdr"`
 }
 
 type GetAssetByIdResponse struct {
 	Data      Coin `json:"data"`
 	Timestamp int  `json:"timestamp"`
+}
+
+type GetAssetsResponse struct {
+	Data      []Coin `json:"data"`
+	Timestamp int    `json:"timestamp"`
 }
 
 func GetAssetById(coinId string) (*Coin, error) {
@@ -38,6 +37,11 @@ func GetAssetById(coinId string) (*Coin, error) {
 	}
 	defer response.Body.Close()
 
+	resStatusCode := response.StatusCode
+	if resStatusCode == 404 {
+		return nil, errors.New("not found")
+	}
+
 	decoder := json.NewDecoder(response.Body)
 	if err := decoder.Decode(&getResponse); err != nil {
 		log.Print(err)
@@ -45,4 +49,29 @@ func GetAssetById(coinId string) (*Coin, error) {
 	}
 
 	return &getResponse.Data, nil
+}
+
+func GetAssets(query map[string]string) ([]Coin, error) {
+	var getResponse GetAssetsResponse
+
+	baseUrl, _ := url.Parse(BASE_URL + "/assets")
+	params := url.Values{}
+	params.Add("limit", query["limit"])
+	params.Add("offset", query["offset"])
+	baseUrl.RawQuery = params.Encode()
+
+	response, err := http.Get(baseUrl.String())
+	if err != nil {
+		log.Print(err)
+		return nil, err
+	}
+	defer response.Body.Close()
+
+	decoder := json.NewDecoder(response.Body)
+	if err := decoder.Decode(&getResponse); err != nil {
+		log.Print(err)
+		return nil, err
+	}
+
+	return getResponse.Data, nil
 }
