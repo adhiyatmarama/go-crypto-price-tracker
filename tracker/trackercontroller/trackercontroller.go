dd08b0f2d8b04af3612af3ad46d6154232c117b0
adhiyatmarama <adhiyatma.pramayoga@gmail.com>
2024-04-22 16:15:48 +0700

    feat: add remove tracker, add price idr, and refactor to tracker service

--- tracker/trackercontroller/trackercontroller.go
+++ tracker/trackercontroller/trackercontroller.go
@@ -2,13 +2,12 @@ package trackercontroller
 
 import (
 	"fmt"
-	"sort"
-	"sync"
+	"log"
 
 	"github.com/adhiyatmarama/go-crypto-price-tracker/database"
-	"github.com/adhiyatmarama/go-crypto-price-tracker/libs/libscoincap"
 	"github.com/adhiyatmarama/go-crypto-price-tracker/middlewares"
 	"github.com/adhiyatmarama/go-crypto-price-tracker/tracker/trackermodel"
+	"github.com/adhiyatmarama/go-crypto-price-tracker/tracker/trackerservice"
 	"github.com/gofiber/fiber/v2"
 )
 
@@ -18,6 +17,7 @@ func GetRoutes() *fiber.App {
 	trackerRoutes.Use(middlewares.JWTMiddleware)
 	trackerRoutes.Get("/", GetTrackersByUser)
 	trackerRoutes.Post("/", CreateTracker)
+	trackerRoutes.Delete("/", RemoveTracker)
 
 	return trackerRoutes
 }
@@ -39,41 +39,26 @@ func CreateTracker(c *fiber.Ctx) error {
 		})
 	}
 
-	existing, err := GetTrackerByUserEmailAndCoinId(userEmail, tracker.CoinId)
+	created, err := trackerservice.CreateTracker(userEmail, tracker)
 	if err != nil {
-		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
-			"message": "Server Error",
-			"error":   err.Error(),
-		})
-	}
-	if len(existing) > 0 {
-		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
-			"message": "Coin has been added to your tracker",
-		})
-	}
-
-	// Add tracker to table
-	_, err = database.DB.Exec(fmt.Sprintf("INSERT INTO Trackers(user_email, coin_id) VALUES('%s', '%s' )", userEmail, tracker.CoinId))
-	if err != nil {
-		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
-			"message": "Error when create tracker to DB",
-			"error":   err.Error(),
-		})
-	}
-
-	created, err := GetTrackerByUserEmailAndCoinId(userEmail, tracker.CoinId)
-	if err != nil {
-		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
-			"message": "Error when create tracker to DB",
-			"error":   err.Error(),
-		})
+		switch err.Error() {
+		case "coin has been added to your tracker":
+			return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+				"message": err.Error(),
+			})
+		default:
+			return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+				"message": "Error when create tracker to DB",
+				"error":   err.Error(),
+			})
+		}
 	}
 
 	return c.Status(fiber.StatusOK).JSON(fiber.Map{
 		"message": "Successfully create a tracker",
 		"tracker": fiber.Map{
-			"user_email": created[0].UserEmail,
-			"coin_id":    created[0].CoinId,
+			"user_email": created.UserEmail,
+			"coin_id":    created.CoinId,
 		},
 	})
 }
@@ -113,76 +98,58 @@ func GetTrackerByUserEmailAndCoinId(userEmail string, coinId string) ([]trackerm
 
 func GetTrackersByUser(c *fiber.Ctx) error {
 	userEmail := fmt.Sprintf("%v", c.Locals("user_email"))
-	result := []*libscoincap.Coin{}
 
-	// Get coin Ids tracked by user
-	rows, err := database.DB.Query(
-		fmt.Sprintf(
-			"select coin_id from Trackers where user_email = '%s'",
-			userEmail,
-		),
-	)
-	if rows == nil {
-		return c.Status(fiber.StatusOK).JSON(fiber.Map{
-			"data": result,
-		})
-	}
+	result, err := trackerservice.GetTrackersByUser(userEmail)
 	if err != nil {
+		log.Print(err)
 		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
-			"message": "Error when get tracker to DB",
-			"error":   err.Error(),
+			"message": "Error when get user tracker",
 		})
 	}
-	defer rows.Close()
-	coinIds := []string{}
-	for rows.Next() {
-		var coin_id string
-		err = rows.Scan(&coin_id)
-		if err != nil {
-			return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
-				"message": "Error when get tracker to DB",
-				"error":   err.Error(),
-			})
-		}
-		coinIds = append(coinIds, coin_id)
-	}
-
-	// Get detail of each coin by coin id concurrently using go routines,  wait group, and channels
-	wg := sync.WaitGroup{}
-	getCoinErrorCh := make(chan error, len(coinIds))
-	coinCh := make(chan *libscoincap.Coin, len(coinIds))
-	for _, coinId := range coinIds {
-		wg.Add(1)
-		go func(coinId string) {
-			defer wg.Done()
-			coin, err := libscoincap.GetAssetById(coinId)
-			if err != nil {
-				getCoinErrorCh <- err
-				return
-			}
-			coinCh <- coin
-		}(coinId)
-	}
-	wg.Wait()
-	close(getCoinErrorCh)
-	close(coinCh)
-
-	// return error if got error
-	if err := <-getCoinErrorCh; err != nil {
-		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
-			"message": "Error when get coin data",
-		})
-	}
-
-	// get coin detail result and sort by coin's rank
-	for coin := range coinCh {
-		result = append(result, coin)
-	}
-	sort.Slice(result, func(i, j int) bool {
-		return result[j].Rank > result[i].Rank
-	})
 
 	return c.Status(fiber.StatusOK).JSON(fiber.Map{
 		"data": result,
+	})
+}
+
+func RemoveTracker(c *fiber.Ctx) error {
+	userEmail := fmt.Sprintf("%v", c.Locals("user_email"))
+
+	var tracker trackermodel.Tracker
+	if err := c.BodyParser(&tracker); err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"message": "bad request",
+			"error":   err.Error(),
+		})
+	}
+
+	if tracker.CoinId == "" {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"message": "Coin id must not be empty",
+		})
+	}
+
+	result, err := database.DB.Exec(
+		fmt.Sprintf(
+			"delete from Trackers where user_email = '%s' and coin_id = '%s'",
+			userEmail,
+			tracker.CoinId,
+		),
+	)
+	if err != nil {
+		log.Print(err.Error())
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"message": "Internal Server Error",
+		})
+	}
+	rowsAffected, _ := result.RowsAffected()
+	if rowsAffected == 0 {
+		return c.Status(fiber.StatusNotFound).JSON(fiber.Map{
+			"message": "Tracker not found",
+		})
+	}
+
+	return c.Status(fiber.StatusOK).JSON(fiber.Map{
+		"message": "successfully remove coin from tracker",
 	})
 }
---
+++ tracker/trackerservice/trackerservice.go
@@ -0,0 +1,141 @@
+package trackerservice
+
+import (
+	"errors"
+	"fmt"
+	"strconv"
+	"sync"
+
+	"github.com/adhiyatmarama/go-crypto-price-tracker/database"
+	"github.com/adhiyatmarama/go-crypto-price-tracker/libs/libscoincap"
+	"github.com/adhiyatmarama/go-crypto-price-tracker/libs/libscurrencyapi"
+	"github.com/adhiyatmarama/go-crypto-price-tracker/tracker/trackermodel"
+)
+
+func CreateTracker(userEmail string, tracker trackermodel.Tracker) (*trackermodel.Tracker, error) {
+	existing, err := GetTrackerByUserEmailAndCoinId(userEmail, tracker.CoinId)
+	if err != nil {
+		return nil, err
+	}
+
+	if len(existing) > 0 {
+		return nil, errors.New("coin has been added to your tracker")
+	}
+
+	// Add tracker to table
+	_, err = database.DB.Exec(fmt.Sprintf("INSERT INTO Trackers(user_email, coin_id) VALUES('%s', '%s' )", userEmail, tracker.CoinId))
+	if err != nil {
+		return nil, err
+	}
+
+	created, err := GetTrackerByUserEmailAndCoinId(userEmail, tracker.CoinId)
+	if err != nil {
+		return nil, err
+	}
+
+	return &created[0], nil
+}
+
+func GetTrackersByUser(userEmail string) ([]libscoincap.Coin, error) {
+	// Get coin Ids tracked by user
+	rows, err := database.DB.Query(
+		fmt.Sprintf(
+			"select coin_id from Trackers where user_email = '%s'",
+			userEmail,
+		),
+	)
+	if rows == nil {
+		return []libscoincap.Coin{}, nil
+	}
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+	coinIds := []string{}
+	for rows.Next() {
+		var coin_id string
+		err = rows.Scan(&coin_id)
+		if err != nil {
+			return nil, err
+		}
+		coinIds = append(coinIds, coin_id)
+	}
+
+	// Get IDR value to USD
+	idrVal, err := libscurrencyapi.GetLatestExchangeRate("USD", "IDR")
+	if err != nil {
+		return nil, err
+	}
+
+	// Get detail of each coin by coin id concurrently using go routines,  wait group, and channels
+	wg := sync.WaitGroup{}
+	result := []libscoincap.Coin{}
+	getCoinErrorCh := make(chan error, len(coinIds))
+	coinCh := make(chan *libscoincap.Coin, len(coinIds))
+	for _, coinId := range coinIds {
+		wg.Add(1)
+		go func(coinId string, idrVal interface{}) {
+			defer wg.Done()
+			coin, err := libscoincap.GetAssetById(coinId)
+			if err != nil {
+				getCoinErrorCh <- err
+				return
+			}
+			usdVal, err := strconv.ParseFloat(coin.PriceUsd, 64)
+			if err != nil {
+				getCoinErrorCh <- err
+				return
+			}
+			coin.PriceIdr = fmt.Sprintf("%.4f", usdVal*idrVal.(float64))
+			coinCh <- coin
+		}(coinId, idrVal)
+	}
+	wg.Wait()
+	close(getCoinErrorCh)
+	close(coinCh)
+
+	// return error if got error
+	if err := <-getCoinErrorCh; err != nil {
+		return nil, err
+	}
+
+	// get coin detail result and sort by coin's rank
+	for coin := range coinCh {
+		result = append(result, *coin)
+	}
+
+	return result, nil
+}
+
+func GetTrackerByUserEmailAndCoinId(userEmail string, coinId string) ([]trackermodel.Tracker, error) {
+	var result []trackermodel.Tracker
+	rows, err := database.DB.Query(
+		fmt.Sprintf(
+			"select user_email, coin_id from Trackers where user_email = '%s' and coin_id = '%s'",
+			userEmail,
+			coinId,
+		),
+	)
+	if rows == nil {
+		return nil, nil
+	}
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+	for rows.Next() {
+		var user_email string
+		var coin_id string
+		err = rows.Scan(&user_email, &coin_id)
+		if err != nil {
+			return nil, err
+		}
+		result = append(result, trackermodel.Tracker{
+			UserEmail: user_email,
+			CoinId:    coin_id,
+		})
+	}
+
+	return result, nil
+
+}
